b8d6762bb1ec8a9cf8cc397bd516ce8d408d3c04
Gad <gad@localhost.localdomain>
2019-08-16 14:48:57 +0200

    feature(landing page): Add a landing page
    - Add free mentors logo
    - Add a navigation bar
    - Add burger menu
    - Add a background image on welcome message block
    - Add sign in and sign up links
    - Add get started links
    - Add other contents and images
    - Add responsiveness to the page
    
    [finishes #48590304]

---
+++ UI/javascript/main.js
@@ -0,0 +1,11 @@
+const menuSlide = () => {
+    const burger = document.querySelector(".menu");
+    const sideMenu = document.querySelector(".side-menu");    
+
+    
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("cross");
+        sideMenu.classList.toggle("slide");
+    })
+}
+menuSlide();
