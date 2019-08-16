1fb61cadb43360e0f785e5374dd60a080d20e07b
Gad <gad@localhost.localdomain>
2019-08-16 17:54:21 +0200

    feature(specific mentor): Add specific mentor page
    - Add mentorship session request option
    - Add reviewing and rate mentor option after mentorship session
    - Add others reviews and ratings of this mentor
    
    [finishes #167946318]

---
+++ UI/javascript/review-mentor.js
@@ -0,0 +1,29 @@
+const enableReview = () =>{
+    const requestButton = document.querySelector("#request-button");
+    const reviewOption = document.querySelector(".review");
+
+    requestButton.addEventListener("click", () => {
+        reviewOption.classList.add("show");
+        requestButton.classList.add("hide");
+    })
+}
+enableReview();
+const rateMentor = () => {
+    const stars = document.querySelectorAll(".rating-star");
+    const rate = (index) => {
+        stars.forEach ((span) => {
+            span.textContent="☆";
+            span.classList.remove("rated");
+          });
+        for(let i=0;i<=index;i++){
+            stars[i].textContent="★";
+            stars[i].classList.add("rated")
+        }
+    }    
+    stars.forEach((star,index) =>{
+        star.addEventListener("click", () =>{
+            rate(index);
+        })
+    })
+}
+rateMentor();
