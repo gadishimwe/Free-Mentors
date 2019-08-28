3f177e22f1a66251b3562ec210f9c12e1a1a94f7
Gad <gad@localhost.localdomain>
2019-08-28 12:58:33 +0200

    feature(reviewMentor):Create reviewMentor endpoint - Add review mentor route - Create review mentor validation middleware - Add review mentor controller - Create reviews model
    [Finishes #168162040]

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -2,6 +2,7 @@
 import sessions from '../models/sessions';
 import users from '../models/users';
 import mentors from '../models/mentors';
+import reviews from '../models/reviews';
 
 exports.sessionRequest = (req, res) => {
   const user = users.find((o) => o.email === req.userData.email);
@@ -70,5 +71,22 @@ exports.allSessions = (req, res) => {
   res.status(200).json({
     status: 200,
     data: sessions.filter((session) => session.menteeId === parseInt(req.userData.userId)),
+  });
+};
+exports.reviewMentor = (req, res) => {
+  const session = sessions.find((sssn) => parseInt(req.params.sessionId) === sssn.sessionId);
+  const user = users.find((usr) => usr.userId === parseInt(req.userData.userId));
+  const newReview = {
+    sessionId: parseInt(req.params.sessionId),
+    mentorId: session.mentorId,
+    menteeId: session.menteeId,
+    score: parseInt(req.body.score),
+    menteeFullName: `${user.firstName} ${user.lastName}`,
+    remark: req.body.remark,
+  };
+  reviews.push(newReview);
+  res.status(200).json({
+    status: 200,
+    data: newReview,
   });
 };
--- server/routes/session.js
+++ server/routes/session.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/named */
 import express from 'express';
-import { sessionRequest, sessionAccept, sessionDecline, allSessions} from '../controllers/sessions';
+import {
+  sessionRequest, sessionAccept, sessionDecline, allSessions, reviewMentor,
+} from '../controllers/sessions';
 import checkAuth from '../middleware/checkAuth';
 import sessionRequestValidator from '../middleware/sessionRequestValidator';
 import checkMentor from '../middleware/checkMentor';
+import reviewMentorValidator from '../middleware/reviewMentorValidator';
 
 const router = express.Router();
 
@@ -10,5 +14,6 @@ router.post('/sessions', [checkAuth, sessionRequestValidator], sessionRequest);
 router.patch('/sessions/:sessionId/accept', [checkAuth, checkMentor], sessionAccept);
 router.patch('/sessions/:sessionId/reject', [checkAuth, checkMentor], sessionDecline);
 router.get('/sessions', checkAuth, allSessions);
+router.post('/sessions/:sessionId/review', [checkAuth, reviewMentorValidator], reviewMentor);
 
 export default router;
