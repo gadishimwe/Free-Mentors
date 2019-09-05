a731d7690f764b0256e1c9329e1b9a4f6ecc13bc
Gad <gad@localhost.localdomain>
2019-09-05 12:21:17 +0200

    feat(root route):Add root route based on feedback - Create root route - Add welcome message with docs link - Add test for this route
    [Finishes #168312256]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import user from './server/routes/user';
 import mentor from './server/routes/mentor';
 import session from './server/routes/session';
+import { link } from 'fs';
 
 
 const app = express();
@@ -14,6 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/', user);
 app.use('/api/v1/', mentor);
 app.use('/api/v1/', session);
+app.get('/', (req, res) => {
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html;charset=utf8');
+  res.end(`<h1>WELCOME TO FREE MENTORS</h1>\nTo get started with Free Mnentors API, use this <a href="https://documenter.getpostman.com/view/8672875/SVfUt7Zy
+  ">documentation.</a>`);
+});
 
 
 const port = process.env.PORT || 3000;
