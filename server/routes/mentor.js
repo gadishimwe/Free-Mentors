06bec5d32a6fcb61f3edf72b701d45aad1ff95c3
Gad <gad@localhost.localdomain>
2019-09-09 21:07:45 +0200

    Chore(Refactoring): Refactoring controllers
    - Remove all conditions from controllers
    - Create separate file for every validation
    
    [Finishes #168380839]

--- server/routes/mentor.js
+++ server/routes/mentor.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { userChangeToMentor, allMentors, specificMentor } from '../controllers/mentor';
 import checkAuth from '../middleware/checkAuth';
 import checkAdmin from '../middleware/checkAdmin';
+import userToMentorValidator from '../middleware/userToMentorValidator';
+import specificMentorExist from '../middleware/specificMentorExist';
 
 const router = express.Router();
 
-router.patch('/user/:userId', [checkAuth, checkAdmin], userChangeToMentor);
+router.patch('/user/:userId', [checkAuth, checkAdmin, userToMentorValidator], userChangeToMentor);
 router.get('/mentors', checkAuth, allMentors);
-router.get('/mentors/:mentorId', checkAuth, specificMentor);
+router.get('/mentors/:mentorId', [checkAuth, specificMentorExist], specificMentor);
 
 export default router;
